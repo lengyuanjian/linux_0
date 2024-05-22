051c8a6b7b1b877ca22d68b361a86308430e5e41
lengyuanjian <1181444531@qq.com>
2024-05-22 10:40:16 +0800

    add vscode make clean tasks launch

---
+++ clear.sh
@@ -0,0 +1,5 @@
+find ../ -type d -name 'build' -exec echo "Delete {}" \; -exec rm -rf {} +
+find ../ -type d -name 'obj' -exec echo "Delete {}" \; -exec rm -rf {} +
+find ../ -type d -name '__pycache__' -exec echo "Delete {}" \; -exec rm -rf {} +
+find ../ -name '*.o' -exec echo "Delete {}" \; -exec rm -rf {} +
+find ../ -name 'app_debug' -exec echo "Delete {}" \; -exec rm -rf {} +
