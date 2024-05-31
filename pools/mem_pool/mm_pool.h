ab513b9c3ddfaed5f4ba98a5607931ccc73c868e
lengyuanjian <1181444531@qq.com>
2024-05-31 17:30:00 +0800

    add mem.h

---
+++ pools/mem_pool/mm_pool.h
@@ -0,0 +1,18 @@
+#ifndef _mm_pool_h_
+#define _mm_pool_h_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct  mem_pools_s
+{
+    ;
+} mem_pools_t;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
